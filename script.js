60848ae3df34934435ea657005429330727ccdb3
Riller <rvsriller@gmail.com>
2021-07-06 19:43:48 -0300

    Fetch

---
+++ script.js
@@ -0,0 +1,17 @@
+// API key compose variable
+
+let apiKey = { key: 'Add your personal key here' };
+
+//GET | fetch Requisition using promises
+fetch(
+  
+)
+  .then((response) => {
+    
+  })
+
+
+
+
+
+  
