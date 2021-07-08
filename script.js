c1aa30775882a9a27f9b271a703470eff273817c
Riller <rvsriller@gmail.com>
2021-07-07 23:17:19 -0300

    Fetch api success

--- script.js
+++ script.js
@@ -18,10 +18,38 @@ fetch(
 
     let info = "";
     
+    // Get 10 coins and symbols
+    for(let i = 0; i < 10 ; i++){
+      
+
+      //Show API info
+      info = info + `
+        <div class="media">
+          <img 
+            src="coin.jpg" 
+            class="align-self-center mr-3" 
+            alt="coin" 
+            width="100" 
+            height="60" 
+          />
+
+          <div class="media-body">
+            <h5 class="mt-2">${api.data[i].name}</h5>
+            <p>${api.data[i].symbol}</p>
+            <p>${api.data[i].first_historical_data}</p>
+          </div>
+        </div>
+      `;
+
+      document.getElementById("coins").innerHTML = texto;
+      
+
+    }
   })
 
   //Error | return error
   .catch((error) => {
+    console.error(error.message);
   });
 
 
