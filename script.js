94ff22572857b72f6bc250eba83bd1dfe2b71cfe
Riller <rvsriller@gmail.com>
2021-07-07 23:34:36 -0300

    Fetch api code

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ fetch(
 
           <div class="media-body">
             <h5 class="mt-2">${api.data[i].name}</h5>
+            <p>${api.data[i].symbol}</p>
           </div>
         </div>
       `;
